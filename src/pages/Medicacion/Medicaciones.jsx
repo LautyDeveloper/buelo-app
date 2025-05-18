7fc1a106707fdda8aeeb8bd62aac1f1599492eff
Lautaro Aquino <laufabio24@gmail.com>
2025-05-17 23:01:34 -0300

    Empezamos Card Medicacion

--- src/pages/Medicacion/Medicaciones.jsx
+++ src/pages/Medicacion/Medicaciones.jsx
@@ -1,5 +1,6 @@
 import Layout from "../../components/Layout/Layout";
 import SectionsHeader from "../../components/Sections-Header/SectionsHeader";
+import Medicacion from "./components/Medicacion/Medicacion";
 import "./Medicaciones.css";
 
 export default function Medicaciones({ theme, setTheme }) {
@@ -10,7 +11,14 @@ export default function Medicaciones({ theme, setTheme }) {
         label={"Agregar Medicacion"}
         color={"var(--medicineFontColor)"}
       />
-      <div className="medicaciones-container"></div>
+      <div className="medicaciones-container">
+        <Medicacion
+          name={"Lorsatan 50mg"}
+          frecuency={"Todos los Dias"}
+          dosis={"2 comprimidos"}
+          schedules={["15:00", "20:00"]}
+        />
+      </div>
     </Layout>
   );
 }
