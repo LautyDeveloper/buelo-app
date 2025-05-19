7266dc18cadaddb5b232e108418ab4937f554158
Lautaro Aquino <laufabio24@gmail.com>
2025-05-19 13:32:49 -0300

    Añadimos Formulario para Agregar una Medicacion

--- src/pages/Medicacion/Medicaciones.jsx
+++ src/pages/Medicacion/Medicaciones.jsx
@@ -1,15 +1,21 @@
+import { AddMedicineForm } from "../../components/AddForms/AddForms";
 import Layout from "../../components/Layout/Layout";
+import ModalForm from "../../components/ModalForm/ModalForm";
 import SectionsHeader from "../../components/Sections-Header/SectionsHeader";
+import useModal from "../../hooks/useModal";
 import Medicacion from "./components/Medicacion/Medicacion";
 import "./Medicaciones.css";
 
 export default function Medicaciones({ theme, setTheme }) {
+  const { isOpen, openModal, closeModal } = useModal();
+
   return (
     <Layout theme={theme} setTheme={setTheme}>
       <SectionsHeader
         title={"Medicaciones"}
         label={"Agregar Medicacion"}
         color={"var(--medicineFontColor)"}
+        openModal={openModal}
       />
       <div className="medicaciones-container">
         <Medicacion
@@ -37,6 +43,16 @@ export default function Medicaciones({ theme, setTheme }) {
           schedules={["15:00", "20:00"]}
         />
       </div>
+      <ModalForm
+        title={"Agregar Medicacion"}
+        parragraph={
+          "Completa los datos de la Medicacion, y no te olvides nunca mas de una Medicacion!"
+        }
+        isOpen={isOpen}
+        onClose={closeModal}
+      >
+        <AddMedicineForm />
+      </ModalForm>
     </Layout>
   );
 }
