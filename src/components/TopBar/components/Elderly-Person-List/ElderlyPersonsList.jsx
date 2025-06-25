451c105bd35852a9a9be61701d8cb72ac159e780
Lautaro Aquino <laufabio24@gmail.com>
2025-06-25 20:49:59 -0300

    Ahora la ElderlyPersonsList, se cierra cuando elegis a la persona.

--- src/components/TopBar/components/Elderly-Person-List/ElderlyPersonsList.jsx
+++ src/components/TopBar/components/Elderly-Person-List/ElderlyPersonsList.jsx
@@ -1,14 +1,20 @@
 import "./elderly-persons-list.css";
 import { useElderlyPerson } from "../../../../context/ElderlyPersonContext.jsx";
 
-export default function ElderlyPersonsList({ persons }) {
+export default function ElderlyPersonsList({ persons, onSelect }) {
   const { setActivePerson } = useElderlyPerson();
 
   return (
     <div className="elderly-persons-list">
       <ul>
         {persons.map((person) => (
-          <li key={person.id} onClick={() => setActivePerson(person)}>
+          <li
+            key={person.id}
+            onClick={() => {
+              setActivePerson(person);
+              onSelect?.(); // <-- Ejecuta la función si está definida
+            }}
+          >
             🤶 {person.nombre}
           </li>
         ))}
