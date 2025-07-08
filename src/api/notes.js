af14f1c0ce277744e9ec497607dac895fcd798c8
Lautaro Aquino <laufabio24@gmail.com>
2025-07-07 23:20:56 -0300

    Funciones add y delete notas

--- src/api/notes.js
+++ src/api/notes.js
@@ -5,3 +5,29 @@ export async function fetchNotes(personId) {
   if (!res.ok) throw new Error("Error al obtener Notas");
   return res.json();
 }
+
+export async function addNote(noteData) {
+  const res = await fetch("http://localhost:3000/notes", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(noteData),
+  });
+
+  if (!res.ok) {
+    const errorData = await res.json();
+    throw new Error(errorData.message || "Error al crear Nota");
+  }
+
+  return res.json();
+}
+
+export async function deleteNote(id) {
+    const res = await fetch(`http://localhost:3000/notes/${id}`, {
+    method: "DELETE",
+  });
+
+  if (!res.ok) throw new Error("Error al eliminar Nota");
+  return res.json();
+}
