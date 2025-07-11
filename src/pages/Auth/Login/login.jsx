301cbd730803f25251b2b9cfc01b780c598297ed
Lautaro Aquino <laufabio24@gmail.com>
2025-07-11 00:04:17 -0300

    Estilos Basicos

--- src/pages/Auth/Login/login.jsx
+++ src/pages/Auth/Login/login.jsx
@@ -4,6 +4,7 @@ export default function Login() {
     <div className="container">
       <div className="login-form-container">
         <div className="form-container">
+          <h1>Iniciar Sesion</h1>
           <form action="">
             <div className="inputs-container">
               <input type="text" placeholder="Nombre de Usuario" />
@@ -11,23 +12,23 @@ export default function Login() {
             </div>
             <input type="submit" value="INICIAR SESION" />
           </form>
-        </div>
-        <div className="other-options">
-          <strong>O, Inicia sesion con:</strong>
-          <div className="options">
-            <div className="option">Google</div>
-            <div className="option">Facebook</div>
-            <div className="option">AppleID</div>
+          <div className="other-options">
+            <strong>O, Inicia sesion con:</strong>
+            <div className="options">
+              <div className="option">Google</div>
+              <div className="option">Facebook</div>
+              <div className="option">AppleID</div>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="aside-container">
-        <div className="aside-header">
-          <p>🤶</p>
-          <p>BueloApp</p>
-        </div>
-        <div className="aside-image">
-          <img src="" alt="" />
+        <div className="aside-container">
+          <div className="aside-header">
+            <p>🤶</p>
+            <p>BueloApp</p>
+          </div>
+          <div className="aside-image">
+            <img src="" alt="" />
+          </div>
         </div>
       </div>
     </div>
